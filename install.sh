679921dccd6e1d5f6cd98631cf087d171a8cf954
Your <184955262+7791372@users.noreply.github.com>
2024-10-22 18:31:37 +0900

    fix output file location

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ if [ "$(id -u)" -eq 0 ]; then
     exit 1
 fi
 
-OUTPUT="$HOME/chat/"
+OUTPUT="$HOME/chat"
 mkdir -p "$OUTPUT"
 
 echo "Downloading Client..."
-curl -sS https://github.com/7791372/chat/releases/latest/download/client-linux \
-    --output "$OUTPUT"
+curl -S https://github.com/7791372/chat/releases/latest/download/client-linux \
+    --output "$OUTPUT/client-linux" \
+    --location
 
 chmod +x "$OUTPUT/client-linux"
 
@@ -20,8 +21,9 @@ read -p "Do you also want to download the server file? (y/N)" SERVER_FILE
 if [[ "$SERVER_FILE" == "y" || "$SERVER_FILE" == "Y" ]]; then
     echo "Downloading Server..."
     curl -sS https://github.com/7791372/chat/releases/latest/download/server-linux \
-        --output ""
-    
+        --output "$OUTPUT/server-linux" \
+        --location
+
     chmod +x "$OUTPUT/server-linux"
 fi
 
