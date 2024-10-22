e2ab09566fc5b15dbd6bf2786df67e0d6a05e681
Your <184955262+7791372@users.noreply.github.com>
2024-10-22 18:05:32 +0900

    fix link oops

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ OUTPUT="$HOME/chat/"
 mkdir -p "$OUTPUT"
 
 echo "Downloading Client..."
-curl -sS https://raw.githubusercontent.com/7791372/chat/releases/latest/download/client-linux \
+curl -sS https://github.com/7791372/chat/releases/latest/download/client-linux \
     --output "$OUTPUT"
 
 chmod +x "$OUTPUT/client-linux"
@@ -19,7 +19,7 @@ read -p "Do you also want to download the server file? (y/N)" SERVER_FILE
 
 if [[ "$SERVER_FILE" == "y" || "$SERVER_FILE" == "Y" ]]; then
     echo "Downloading Server..."
-    curl -sS https://raw.githubusercontent.com/7791372/chat/releases/latest/download/server-linux \
+    curl -sS https://github.com/7791372/chat/releases/latest/download/server-linux \
         --output ""
     
     chmod +x "$OUTPUT/server-linux"
