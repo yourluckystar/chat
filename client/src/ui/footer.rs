ef17e365c0fcb31cadc65c05179f682886af373c
Your <184955262+yourluckystar@users.noreply.github.com>
2024-10-24 20:47:50 +0900

    remove comment

--- client/src/ui/footer.rs
+++ client/src/ui/footer.rs
@@ -4,8 +4,6 @@ use crossterm::terminal::size;
 pub fn draw() {
     let (_width, height) = size().unwrap();
 
-    // Draw input prompt at the bottom
-    // print!("\x1B[{};1H\x1B[2mkonata is typing\x1B[0m", height);
     print!("\x1B[{};1H$", height);
     io::stdout().flush().unwrap();
 }
